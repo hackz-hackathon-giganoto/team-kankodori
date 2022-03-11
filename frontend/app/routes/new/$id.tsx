1c4c1011c3d76f2e2b26be825762938f4849ff9e
ssssota <tommysouta@gmail.com>
2022-03-11 17:49:03 +0900

    暫定で動作確認ページ

---
+++ frontend/app/routes/new/$id.tsx
@@ -0,0 +1,9 @@
+import { SvgCanvas } from "~/components/SvgCanvas";
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <SvgCanvas />
+    </div>
+  );
+}
