0068175d392b0c7377d30d3baeaca0689a91f6af
nori3636 <nori3636@gmail.com>
2022-03-13 00:03:47 +0900

    outletを試す

--- frontend/app/routes/inols/$pageId.tsx
+++ frontend/app/routes/inols/$pageId.tsx
@@ -1,11 +1,13 @@
-import { Link } from "remix";
+import { Link, Outlet } from 'remix';
 
 export default function Inols() {
   return (
     <main>
       <h1>一覧を見るページ</h1>
       <p>グリッドで表示</p>
+
       <Link to="/inol/1">最初のイノりを見る</Link>
+      <Outlet />
     </main>
   );
 }
---
+++ frontend/app/routes/inols/$pageId/index.tsx
@@ -0,0 +1,7 @@
+export default function OutletTest() {
+  return (
+    <div>
+      <h1>Outletで出てくるページ</h1>
+    </div>
+  );
+}
