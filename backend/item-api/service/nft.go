32a43f4e04817b3666673a17ca6b059e9e4cb9af
Takamichi Omori <merrytakaemerry@gmail.com>
2022-03-15 02:28:48 +0900

    impl CreateNonfungible

--- backend/item-api/service/nft.go
+++ backend/item-api/service/nft.go
@@ -7,8 +7,33 @@ import (
 	"github.com/hackz-hackathon-giganoto/team-kankodori/backend/item-api/api"
 )
 
-func CreateNonFungible(name, contractID string) (*TransactionAccepted, error) {
-	path := fmt.Sprintf("/v1/item-tokens/%s/non-fungibles/", contractID)
+func GetTransaction(txHash string) (*Transaction, error) {
+	path := fmt.Sprintf("/v1/transactions/%s", txHash)
+
+	apiResult, err := api.CallAPI(path, "GET", nil, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(string(apiResult))
+	tx := Transaction{}
+	if err := json.Unmarshal(apiResult, &tx); err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	return &tx, nil
+}
+
+func CreateNonFungible(name string) (*TransactionAccepted, error) {
+	path := fmt.Sprintf("/v1/item-tokens/%s/non-fungibles/", mustGetEnv("ITEM_CONTRACT_ID"))
+
+	params := map[string]interface{}{
+		"name":         name,
+		"ownerAddress": mustGetEnv("WALLET_ADDRESS"),
+		"ownerSecret":  mustGetEnv("WALLET_SECRET"),
+	}
+
 
 	params := map[string]interface{}{
 		"name":         name,
