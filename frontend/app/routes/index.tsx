cb2eac3b235d8704d66bd9248c793f245f5c5e41
ssssota <tommysouta@gmail.com>
2022-03-14 09:55:37 +0900

    overflow hidden

--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -2,7 +2,7 @@ import { CatchPhrase } from '~/components/CatchPhrase';
 
 export default function Index() {
   return (
-    <main className="bg-gradient-to-b from-[#f3d9a0] to-[#f7db8e] min-h-screen relative">
+    <main className="bg-gradient-to-b from-[#f3d9a0] to-[#f7db8e] min-h-screen relative max-h-screen overflow-hidden">
       <CatchPhrase className="absolute top-0 w-full -translate-y-full rotate-90 origin-bottom-left" />
     </main>
   );
