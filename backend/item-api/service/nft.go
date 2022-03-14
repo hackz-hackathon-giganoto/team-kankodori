3354e2ec4ac330ed1c55650abbf49f574de9828a
Takamichi Omori <merrytakaemerry@gmail.com>
2022-03-15 01:57:24 +0900

    impl CreateNonFungible

--- backend/item-api/service/nft.go
+++ backend/item-api/service/nft.go
@@ -1,1 +1,31 @@
 package service
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/hackz-hackathon-giganoto/team-kankodori/backend/item-api/api"
+)
+
+func CreateNonFungible(name, contractID string) (*TransactionAccepted, error) {
+	path := fmt.Sprintf("/v1/item-tokens/%s/non-fungibles/", contractID)
+
+	params := map[string]interface{}{
+		"name":         name,
+		"ownerAddress": mustGetEnv("WALLET_ADDRESS"),
+		"ownerSecret":  mustGetEnv("WALLET_SECRET"),
+	}
+
+	apiResult, err := api.CallAPI(path, "POST", nil, params)
+	if err != nil {
+		return nil, err
+	}
+
+	txAccepted := &TransactionAccepted{}
+
+	if err := json.Unmarshal(apiResult, txAccepted); err != nil {
+		return nil, err
+	}
+
+	return txAccepted, nil
+}
