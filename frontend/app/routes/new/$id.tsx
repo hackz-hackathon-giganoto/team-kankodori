727cc2527792cad81ea95f269d17b59e81f38067
ssssota <tommysouta@gmail.com>
2022-03-14 17:11:25 +0900

    add bg selector

--- frontend/app/routes/new/$id.tsx
+++ frontend/app/routes/new/$id.tsx
@@ -1,6 +1,6 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { json, LoaderFunction, useLoaderData } from 'remix';
-import { Padlock } from '~/components/baseSvg';
+import { Ema, Padlock } from '~/components/baseSvg';
 import { Selector } from '~/components/Selector';
 import { SvgCanvas } from '~/components/SvgCanvas';
 import {
@@ -15,7 +15,7 @@ export const loader: LoaderFunction = async ({ params }) => {
 
 export default function Index() {
   const [mode, setMode] = useState<number>(0);
-  const onModeChange = useCallback((i: number) => setMode(i), []);
+  const [background, setBackground] = useState<number>(0);
   const id = useLoaderData<string>();
   const controller = useMemo<NetworkController>(
     () =>
@@ -35,7 +35,7 @@ export default function Index() {
     <div>
       <SvgCanvas
         networkController={controller}
-        BackgroundSvg={Padlock}
+        BackgroundSvg={background === 0 ? Ema : Padlock}
         mode={mode === 0 ? 'pen' : 'eraser'}
         className="h-[90vmin] w-[90vmin] mx-auto bg-white/50 rounded-xl"
       />
@@ -53,7 +53,23 @@ export default function Index() {
             },
           ]}
           name="tool"
-          onChange={onModeChange}
+          onChange={setMode}
+          className="bg-white/50"
+        />
+        <Selector
+          selectedIndex={background}
+          items={[
+            {
+              el: () => <span className="px-4 py-2">絵馬</span>,
+              key: 'ema',
+            },
+            {
+              el: () => <span className="px-4 py-2">南京錠</span>,
+              key: 'padlock',
+            },
+          ]}
+          name="background"
+          onChange={setBackground}
           className="bg-white/50"
         />
       </div>
