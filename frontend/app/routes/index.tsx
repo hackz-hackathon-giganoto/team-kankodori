4f69dcbcc6f73cbcbaa5b1899167ce15fd4b7615
ssssota <tommysouta@gmail.com>
2022-03-14 13:58:18 +0900

    fix order className

--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -4,7 +4,7 @@ import { Menu } from '~/components/Menu';
 
 export default function Index() {
   return (
-    <main className="bg-gradient-to-b from-[#fae5b9] to-[#f7db8e] min-h-screen relative max-h-screen overflow-hidden">
+    <main className="bg-gradient-to-b from-[#fae5b9] to-[#f7db8e] relative min-h-screen max-h-screen overflow-hidden">
       <Hinomaru className="absolute -top-1/4 -right-1/4 w-[110%]" />
       <CatchPhrase className="absolute top-0 w-full -translate-y-full rotate-90 origin-bottom-left" />
       <Menu className="absolute bottom-0 right-0" />
