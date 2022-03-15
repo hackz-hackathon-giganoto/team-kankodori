395663ea4da26592612560173e4116b760b1b175
nori3636 <nori.k3636@gmail.com>
2022-03-15 15:51:23 +0900

    descriptionの変更

--- frontend/app/routes/items/$country/$pref/$city.tsx
+++ frontend/app/routes/items/$country/$pref/$city.tsx
@@ -35,7 +35,7 @@ export const loader: LoaderFunction = async ({ params }) => {
 export const meta: MetaFunction = ({ data }) => {
   const ogImage = `https://inol.cf/png/${data.id}`;
   const description =
-    'PRAY WITH YOU, PRAY WITH ME.インターネット神社inolで誓いを立てました';
+    'PRAY WITH YOU, PRAY WITH ME. インターネット神社inolで誓いを立てました';
   return {
     'application-name': 'inol',
     title: 'inolで誓いを立てました',
