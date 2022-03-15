a820571d2d9941a1edb1f9e5dde5338eae9e6e7b
nori3636 <nori.k3636@gmail.com>
2022-03-16 00:28:19 +0900

    アラートの削除

--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -1,17 +1,8 @@
-import { useEffect } from 'react';
 import { CatchPhrase } from '~/components/CatchPhrase';
 import { Circle } from '~/components/Circle';
 import { Menu } from '~/components/Menu';
-import { useLiffUserId } from '~/utils/liff';
 
 export default function Index() {
-  const userId = useLiffUserId();
-  useEffect(() => {
-    if (userId !== undefined) {
-      alert(userId);
-    }
-  }, [userId]);
-
   return (
     <main>
       <Circle className="fixed -top-1/4 -right-1/4 w-[110%] bg-red-600" />
