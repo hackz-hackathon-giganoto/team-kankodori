dcc7d714a07883bce6a5ab69ee38d44cbb98e211
nori3636 <nori.k3636@gmail.com>
2022-03-16 08:52:13 +0900

    titleの追加

--- frontend/app/routes/items/$country/$pref/$city.tsx
+++ frontend/app/routes/items/$country/$pref/$city.tsx
@@ -35,7 +35,8 @@ export const loader: LoaderFunction = async ({ params }) => {
 export const meta: MetaFunction = ({ data }) => {
   const ogImage = `https://inol.cf/png/${data.id}`;
   return {
-    title: 'inol',
+    title: 'inolを見つけました',
+    'og:title': 'inol',
     'og:image': ogImage,
     'twitter:image': ogImage,
   };
