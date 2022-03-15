dfa07a38466fe5704a85b982f3316b63218f3009
ssssota <tommysouta@gmail.com>
2022-03-16 03:44:19 +0900

    fix background opacity

--- frontend/app/components/ItemDetails.tsx
+++ frontend/app/components/ItemDetails.tsx
@@ -3,7 +3,7 @@ import { Item } from '~/data/types';
 
 export const ItemDetails: VFC<{ item: Item }> = ({ item }) => (
   <article
-    className="w-11/12 px-4 py-4 bg-slate-50/80 rounded-2xl"
+    className="w-11/12 px-4 py-4 bg-slate-50 rounded-2xl"
     onClick={(e) => e.stopPropagation()}
   >
     <h3 className="text-2xl font-bold">{item.name}</h3>
