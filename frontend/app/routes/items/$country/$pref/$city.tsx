b04895e78d78111f54cf6ac7b14977d68bcd791a
nori3636 <nori.k3636@gmail.com>
2022-03-15 14:32:12 +0900

    共有用OGPの追加

--- frontend/app/routes/items/$country/$pref/$city.tsx
+++ frontend/app/routes/items/$country/$pref/$city.tsx
@@ -34,19 +34,18 @@ export const loader: LoaderFunction = async ({ params }) => {
 
 export const meta: MetaFunction = ({ data }) => {
   const ogImage = `https://inol.cf/png/${data.id}`;
+  const description =
+    'PRAY WITH YOU, PRAY WITH ME.インターネット神社inolで誓いを立てました';
   return {
     'application-name': 'inol',
     title: 'inolで誓いを立てました',
-    description:
-      'PRAY WITH YOU, PRAY WITH ME.インターネット神社inolで誓いを立てました',
+    description,
     'og:title': 'inol',
-    'og:description':
-      'PRAY WITH YOU, PRAY WITH ME.インターネット神社inolで誓いを立てました',
+    'og:description': description,
     'og:image': ogImage,
     'twitter:card': 'summary_large_image',
     'twitter:title': 'inol',
-    'twitter:description':
-      'PRAY WITH YOU, PRAY WITH ME.インターネット神社inolで誓いを立てました',
+    'twitter:description': description,
     'twitter:image': ogImage,
   };
 };
