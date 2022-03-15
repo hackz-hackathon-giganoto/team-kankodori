21fd5d530a23ce14adab6465ed5bbb1656a86825
ssssota <tommysouta@gmail.com>
2022-03-15 10:53:06 +0900

    add share button

--- frontend/app/routes/new/$id.tsx
+++ frontend/app/routes/new/$id.tsx
@@ -1,6 +1,7 @@
 import { useMemo, useState } from 'react';
 import { json, LoaderFunction, useLoaderData } from 'remix';
 import { Ema, Padlock } from '~/components/baseSvg';
+import { Button } from '~/components/Button';
 import { Selector } from '~/components/Selector';
 import { SvgCanvas } from '~/components/SvgCanvas';
 import {
@@ -72,6 +73,7 @@ export default function Index() {
           onChange={setBackground}
           className="bg-white/50"
         />
+        <Button onClick={() => window.navigator.share()}>一緒に描く</Button>
       </div>
     </div>
   );
