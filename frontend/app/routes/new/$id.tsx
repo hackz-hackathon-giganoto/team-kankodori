fab850f69a33b2a245b46cf459c2e49f091f0336
ssssota <tommysouta@gmail.com>
2022-03-15 11:00:04 +0900

    add share data

--- frontend/app/routes/new/$id.tsx
+++ frontend/app/routes/new/$id.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from 'react';
-import { json, LoaderFunction, useLoaderData } from 'remix';
+import { json, LoaderFunction, useLoaderData, useLocation } from 'remix';
 import { Ema, Padlock } from '~/components/baseSvg';
 import { Button } from '~/components/Button';
 import { Selector } from '~/components/Selector';
@@ -73,7 +73,17 @@ export default function Index() {
           onChange={setBackground}
           className="bg-white/50"
         />
-        <Button onClick={() => window.navigator.share()}>一緒に描く</Button>
+        <Button
+          onClick={() =>
+            window.navigator.share({
+              url: window.location.href,
+              text: '一緒に祈りましょう！',
+              title: 'PRAY WITH YOU - Inol',
+            })
+          }
+        >
+          一緒に描く
+        </Button>
       </div>
     </div>
   );
